9881b89193069b39c96e976ca2c63192d89c0e82
Benjamin <benjaminkahl19@gmail.com>
2017-06-16 02:23:46 +0200

    Basic Camera Controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float scrollBoundary = 50;
+	public float scrollSpeed = 5;
+
+	void Start () {
+
+	}
+
+	void Update() {
+		if(Input.mousePosition.x > Screen.width - scrollBoundary) {
+			transform.Translate(0, 0, scrollSpeed * Time.deltaTime);
+		}
+		if(Input.mousePosition.x < 0 + scrollBoundary) {
+			transform.Translate(0, 0, -scrollSpeed * Time.deltaTime);
+		}
+		if(Input.mousePosition.y > Screen.height - scrollBoundary) {
+			transform.Translate(-scrollSpeed * Time.deltaTime, 0, 0);
+		}
+		if(Input.mousePosition.y < 0 + scrollBoundary) {
+			transform.Translate(scrollSpeed * Time.deltaTime, 0, 0);
+		}
+	}
+}
