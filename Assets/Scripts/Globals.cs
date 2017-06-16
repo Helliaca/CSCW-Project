6410b21560a9d55cf03eacde563c2a38e0e291c5
Benjamin <benjaminkahl19@gmail.com>
2017-06-16 02:20:11 +0200

    Minimal runnable version

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals : MonoBehaviour {
+
+	public static Globals Data;
+
+	void Awake()
+	{
+		if(Data != null) GameObject.Destroy(Data);
+		else Data = this;
+		DontDestroyOnLoad(this);
+	}
+		
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TerritoryController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerritoryController : MonoBehaviour {
+
+	private static Color color_default = new Color(0.7647f, 0.7647f, 0.7647f, 1f);
+	private static Color color_selected = new Color(0.854f, 0.814f, 0.814f, 0.5f);
+	//private static Color color_claimed = new Color(1f, 0.5f, 0.5f, 1f);
+
+	public TerritoryController[] Neighbours;
+
+	void OnMouseEnter() {
+		Renderer rend = GetComponent<Renderer>();
+		rend.material.SetColor("_Color", color_selected);
+	}
+
+	void OnMouseExit() {
+		Renderer rend = GetComponent<Renderer>();
+		rend.material.SetColor("_Color", color_default);
+	}
+}
