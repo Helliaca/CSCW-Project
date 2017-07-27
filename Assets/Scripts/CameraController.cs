7d48530c861643253fb5a3c49a57b08bc3db5cc7
Benjamin <benjaminkahl19@gmail.com>
2017-07-27 18:16:41 +0200

    Added flag prefab

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,22 +6,33 @@ public class CameraController : MonoBehaviour {
 
 	public float scrollBoundary = 50;
 	public float scrollSpeed = 5;
+	public float rotationSpeedHorizontal = 2.0f;
+	public float rotationSpeedVertical = 2.0f;
+
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
 
 	void Start () {
 
 	}
 
 	void Update() {
-		if(Input.mousePosition.x > Screen.width - scrollBoundary) {
+		if(Input.GetKey(KeyCode.Mouse1)) {
+			yaw += rotationSpeedHorizontal * Input.GetAxis("Mouse X");
+			pitch -= rotationSpeedVertical * Input.GetAxis("Mouse Y");
+			transform.eulerAngles = new Vector3(0, yaw, pitch);
+			return;
+		}
+		if(Input.mousePosition.x > Screen.width - scrollBoundary || Input.GetKey(KeyCode.RightArrow)) {
 			transform.Translate(0, 0, scrollSpeed * Time.deltaTime);
 		}
-		if(Input.mousePosition.x < 0 + scrollBoundary) {
+		if(Input.mousePosition.x < 0 + scrollBoundary || Input.GetKey(KeyCode.LeftArrow)) {
 			transform.Translate(0, 0, -scrollSpeed * Time.deltaTime);
 		}
-		if(Input.mousePosition.y > Screen.height - scrollBoundary) {
+		if(Input.mousePosition.y > Screen.height - scrollBoundary || Input.GetKey(KeyCode.UpArrow)) {
 			transform.Translate(-scrollSpeed * Time.deltaTime, 0, 0);
 		}
-		if(Input.mousePosition.y < 0 + scrollBoundary) {
+		if(Input.mousePosition.y < 0 + scrollBoundary || Input.GetKey(KeyCode.DownArrow)) {
 			transform.Translate(scrollSpeed * Time.deltaTime, 0, 0);
 		}
 	}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+	public float staticX = 0;
+	public float amplitude = 0.005f;
+	public float speed = 0.015f;
+	public float waveAmount = 200;
+	public float dampening = 0.1f;
+
+	Vector3[] verts_original;
+	Vector3[] verts_deformed;
+
+	MeshFilter mesh;
+
+	// Use this for initialization
+	void Start () {
+		mesh = GetComponent<MeshFilter>();
+		verts_original = (Vector3[]) mesh.mesh.vertices.Clone();
+		verts_deformed = (Vector3[]) verts_original.Clone();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i=0; i<verts_deformed.Length; i++) {
+			float maxVal = amplitude * Mathf.Sin((Time.time*speed+verts_deformed[i].x)*waveAmount);
+			verts_deformed[i].z = verts_original[i].z + Mathf.Lerp(0, maxVal, (Mathf.Abs(verts_deformed[i].x-staticX))/dampening);
+		}
+		mesh.mesh.vertices = verts_deformed;
+		mesh.mesh.RecalculateNormals();
+	}
+}
